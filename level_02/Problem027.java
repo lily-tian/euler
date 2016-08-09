62740c6e4111d05722d3a11fdef7666572b7bbb6
Lily <lily.tianyuan@gmail.com>
2016-08-08 23:18:53 -0700

    Applied encryption

--- level_02/Problem027.java
+++ level_02/Problem027.java
@@ -24,6 +24,8 @@
  *Find the product of the coefficients, a and b, for the
  *quadratic expression that produces the maximum number of
  *primes for consecutive values of n, starting with n = 0.
+ *
+ *Dependencies: LilyCalcs
  */
 
 public class Problem027 {
